f97b95a89ed59f3d665bbe9ee80cedae455143bd
oli <72987619+olivieshek@users.noreply.github.com>
2023-01-29 11:29:05 +0300

    "view site" links to the posts list in blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, ModelCategory, Like
 
+admin.site.site_url = '/blog/'
 admin.site.register(Post)
 admin.site.register(ModelCategory, admin.ModelAdmin)
 admin.site.register(Like)
